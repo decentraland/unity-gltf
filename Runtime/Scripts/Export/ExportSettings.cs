atteneder/glTFast

feat: Added KHR_lights_punctual support for export (#470)

Runtime/Scripts/Export/ExportSettings.cs
@@ -77,5 +77,11 @@ namespace GLTFast.Export {
         
         /// <inheritdoc cref="FileConflictResolution"/>
         public FileConflictResolution fileConflictResolution = FileConflictResolution.Abort;
+        
+        /// <summary>
+        /// Light intensity values are divided by this factor.
+        /// </summary>
+        [Tooltip("Light intensity values are divided by this factor")]
+        public float lightIntensityFactor = 1.0f;
     }
 }

decentraland/unity-gltf

Runtime/Scripts/Export/ExportSettings.cs
@@ -77,5 +77,11 @@ namespace GLTFast.Export {
         
         /// <inheritdoc cref="FileConflictResolution"/>
         public FileConflictResolution fileConflictResolution = FileConflictResolution.Abort;
+        
+        /// <summary>
+        /// Light intensity values are multiplied by this factor.
+        /// </summary>
+        [Tooltip("Light intensity values are multiplied by this factor")]
+        public float lightIntensityFactor = 1.0f;
     }
 }